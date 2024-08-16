3123dfaf36bbb62055efd7c1835f431e8469c134
LysenkovIlya <lysenkov_iv21@mail.ru>
2024-08-16 20:32:13 +0300

    Исправлена game_progression

--- brain_games/scripts/brain_progression.py
+++ brain_games/scripts/brain_progression.py
@@ -4,14 +4,14 @@ from brain_games.cli import welcome_user, ask_question
 DESCRIPTION = 'What number is missing in the progression?'
 
 def generate_question_and_answer():
-    progression_length = random.randint(5, 10)  # Длина прогрессии от 5 до 10
-    start = random.randint(1, 20)  # Начальное число прогрессии
-    step = random.randint(1, 10)  # Шаг прогрессии
+    progression_length = random.randint(5, 10)
+    start = random.randint(1, 20)
+    step = random.randint(1, 10)
 
     progression = [start + step * i for i in range(progression_length)]
     missing_index = random.randint(0, progression_length - 1)
     missing_number = progression[missing_index]
-    progression[missing_index] = '..'  # Заменяем число на ..
+    progression[missing_index] = '..'
 
     question = ' '.join(map(str, progression))
     return question, str(missing_number)
@@ -23,6 +23,7 @@ def main():
     
     for _ in range(3):
         question, correct_answer = generate_question_and_answer()
+        print(f'Question: {question}')
         user_answer = ask_question(question)
         
         if user_answer == correct_answer:
